3188b6c1f2d8d3fcb57b06b877b2f83380283958
LeeJaeJun-A <sjaqjnjs@naver.com>
2025-02-03 17:24:15 +0900

    fix: login component route

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Login from "$lib/components/login/login_private/Login.svelte";
+  import Login from "$lib/components/login/Login.svelte";
   import Loading from "$lib/components/Loading.svelte";
   import { onMount } from "svelte";
   import { browser } from "$app/environment";
